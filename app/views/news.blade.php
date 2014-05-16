9bab015230ecea651870c1a7627351542879398c
Guydo123 <gjfc50@yahoo.co.uk>
2014-05-16 16:07:56 +0100

    first drafr td

---
+++ app/views/news.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+
+@section('title')
+welcome to PHP Laravel
+@stop
+
+@section('content')
+
+<div class="small-12 columns panel">
+<h2>Welcome</h2>
+<p>This is the news page</p>
+
+</div>
+
+@foreach($news as $item)
+<div class="small-6 columns panel">
+    <h3>{{ $item->title }}</h3>
+    <p>{{ $item->details }}</p>
+</div>
+@endforeach
+
+
+@stop
+
+@section('footer')
+@parent
+<p>All rights reserved.</p>
+@stop
