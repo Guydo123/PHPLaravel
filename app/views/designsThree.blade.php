4502dcbd58649fd44062661f822dcc9555c5766b
Guydo123 <gjfc50@yahoo.co.uk>
2014-05-23 14:42:18 +0100

    new pics

--- app/views/designsThree.blade.php
+++ app/views/designsThree.blade.php
@@ -12,38 +12,44 @@ Designs
 <h3 class="filterD"><a href="index.php/designsThree">Farm</a></h3>
 <h3 class="filterD"><a href="index.php/designsFour">Reptilez</a></h3>
 <div class="columns panel">
-    <h2>Africa</h2>
+    <h2>Farm Collection</h2>
+    <div class="row">
+    <div class="large-6 medium-6 columns">
+        <a class="single_image"  href="img/lightbox/Horse.gif" title="The Horse - Farm Collection.">
+            <img src="img/thumbs/Horsethumb.gif" alt="horse" />
+        </a>
+        <p>The Horse</p></div>
+    <div class="large-6 medium-6 columns">
+        <a class="single_image"  href="img/lightbox/Bull.gif" title="The Bull - Farm Collection.">
+            <img src="img/thumbs/Bullthumb.gif" alt="Bull" />
+        </a>
+        <p>The Bull</p></div>
+        </div>
+        <div class="row">
+    <div class="large-6 medium-6 columns">
+        <a class="single_image"  href="img/lightbox/Cockerel.gif" title="The Cockerel - Farm Collection.">
+            <img src="img/thumbs/Cockerelthumb.gif" alt="Cockerel" />
+        </a>
+        <p>The Cockerel</p></div>
+    <div class="large-6 medium-6 columns">
+        <a class="single_image"  href="img/lightbox/Pig.gif" title="The Pig - Farm Collection.">
+            <img src="img/thumbs/Pigthumb.gif" alt="pig" />
+        </a>
+        <p>The Pig</p></div>
+            </div>
+            <div class="row">
     <div class="large-6 medium-6 columns">
         <a class="single_image"  href="img/lightbox/rhino.jpg" title="Rhino - Africa Collection.">
             <img src="img/thumbs/rhinothumbnail.jpg" alt="rhino" />
         </a>
-        <p>The Rhino</p></div>
-    <div class="large-6 medium-6 columns">
-        <a class="single_image"  href="img/lightbox/rhino.jpg" title="Rhino - Africa Collection.">
-            <img src="img/thumbs/rhinothumbnail.jpg" alt="rhino" />
-        </a>
-        <p>The Rhino</p></div>
-    <div class="large-6 medium-6 columns">
-        <a class="single_image"  href="img/lightbox/rhino.jpg" title="Rhino - Africa Collection.">
-            <img src="img/thumbs/rhinothumbnail.jpg" alt="rhino" />
-        </a>
-        <p>The Rhino</p></div>
-    <div class="large-6 medium-6 columns">
-        <a class="single_image"  href="img/lightbox/rhino.jpg" title="Rhino - Africa Collection.">
-            <img src="img/thumbs/rhinothumbnail.jpg" alt="rhino" />
-        </a>
-        <p>The Rhino</p></div>
-    <div class="large-6 medium-6 columns">
-        <a class="single_image"  href="img/lightbox/rhino.jpg" title="Rhino - Africa Collection.">
-            <img src="img/thumbs/rhinothumbnail.jpg" alt="rhino" />
-        </a>
-        <p>The Rhino</p></div>
-    <div class="large-6 medium-6 columns">
-        <a class="single_image"  href="img/lightbox/rhino.jpg" title="Rhino - Africa Collection.">
-            <img src="img/thumbs/rhinothumbnail.jpg" alt="rhino" />
-        </a>
-        <p>The Rhino</p></div>
 
+        <p>The Rhino</p></div>
+    <div class="large-6 medium-6 columns">
+        <a class="single_image"  href="img/lightbox/rhino.jpg" title="Rhino - Africa Collection.">
+            <img src="img/thumbs/rhinothumbnail.jpg" alt="rhino" />
+        </a>
+        <p>The Rhino</p></div>
+</div>
 
 </div>
 
